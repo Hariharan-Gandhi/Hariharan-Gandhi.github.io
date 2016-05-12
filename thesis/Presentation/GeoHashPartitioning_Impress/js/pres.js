c8dd4fe47db992e1fd9f26f4b7b4e2225c69f5c6
Hariharan Gandhi <Hariharan-Gandhi@users.noreply.github.com>
2016-05-12 22:10:22 +0200

    Update pres.js

--- thesis/Presentation/GeoHashPartitioning_Impress/js/pres.js
+++ thesis/Presentation/GeoHashPartitioning_Impress/js/pres.js
@@ -14,7 +14,7 @@ function launch(){
 
     var pwd = $("#password").val();
 
-    var url = "https://raw.githubusercontent.com/Hariharan-Gandhi/Master-Thesis/master/"+pwd+"?token=AJMsgN1aBww9moTiIk526ObFRnXz9g75ks5XKf-mwA%3D%3D" ;
+    var url = "https://raw.githubusercontent.com/Hariharan-Gandhi/Master-Thesis/master/"+pwd+"?token=AJMsgPX952mTtHkzKw-Ma-je0Svc1NYlks5XPh0vwA%3D%3D" ;
 
     $.get(url, function(data) {
     	
